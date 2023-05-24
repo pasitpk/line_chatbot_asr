78240683f0418e2146f108f9675a5c8f4376249f
pasitpk <pasit.j1995@gmail.com>
2023-05-25 01:14:15 +0700

    add new endpoit for transcribing

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from transformers import pipeline
 
 from typing import List, Optional
 
-from fastapi import HTTPException, Header, Request, FastAPI
+from fastapi import HTTPException, Header, Request, FastAPI, File, UploadFile
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import AudioMessage, MessageEvent, TextSendMessage
@@ -22,6 +22,7 @@ with open('.json', 'r') as f:
 line_bot_api = LineBotApi(config['LINE_CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(config['LINE_CHANNEL_SECRET'])
 asr_pipe = config['ASR_PIPE']
+asr_pipe2 = config['ASR_PIPE2']
 
 with open(config['CUSTOM_DICT'], 'rb') as f:
     custom_dict = pickle.load(f)
@@ -36,6 +37,11 @@ pipe = pipeline("automatic-speech-recognition",
                     device=device,
                     )
 
+pipe2 = pipeline("automatic-speech-recognition",
+                    model=asr_pipe2,
+                    device=device,
+                    )
+
 app = FastAPI()
 
 class Line(BaseModel):
@@ -46,6 +52,17 @@ class Line(BaseModel):
 @app.get("/")
 async def root():
     return {"message": "Hello, DeepGI"}
+
+
+@app.post("/transcribe/")
+async def transcribe_file(file: UploadFile = File(...)):
+    file.file.seek = lambda *args: None
+    audio = pydub.AudioSegment.from_file(file.file)
+    text = pipe2(audio.export(format='wav').read())['text']
+    res = {
+        'text': text
+    }
+    return res
 
 
 @app.post("/linehook")
@@ -69,9 +86,8 @@ def message_text(event):
 
 
 def transcribe(message_content):
-    audio, sampling_rate = get_audio(message_content)
-    audio = audio.mean(1)
-    text = pipe({'raw': audio, 'sampling_rate': sampling_rate})['text']
+    audio = get_audio(message_content)
+    text = pipe(audio.export(format='wav').read())['text']
     text = corrector.correct(text)
     return text
 
@@ -82,15 +98,5 @@ def get_audio(message_content):
             fd.write(chunk)
         fd.seek(0)
         audio = pydub.AudioSegment.from_file(fd)
-    audio, sampling_rate = pydub_to_np(audio)
-    return audio, sampling_rate
+    return audio
     
-
-def pydub_to_np(audio):
-    """
-    Converts pydub audio segment into np.float32 of shape [duration_in_seconds*sample_rate, channels],
-    where each value is in range [-1.0, 1.0]. 
-    Returns tuple (audio_np_array, sample_rate).
-    """
-    return np.array(audio.get_array_of_samples(), dtype=np.float32).reshape((-1, audio.channels)) / (
-            1 << (8 * audio.sample_width - 1)), audio.frame_rate
