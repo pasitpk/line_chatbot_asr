8e3109cdf99e80ed664ba552aa6638164c708940
pasitpk <pasit.j1995@gmail.com>
2023-08-30 11:03:20 +0700

    add correction as config

--- main.py
+++ main.py
@@ -19,26 +19,26 @@ from corrector import DeepGICorrector
 with open('.json', 'r') as f:
     config = json.load(f)
 
-line_bot_api = LineBotApi(config['LINE_CHANNEL_ACCESS_TOKEN'])
-handler = WebhookHandler(config['LINE_CHANNEL_SECRET'])
-asr_pipe = config['ASR_PIPE']
-asr_pipe2 = config['ASR_PIPE2']
+LINE_BOT_API = LineBotApi(config['LINE_CHANNEL_ACCESS_TOKEN'])
+HANDLER = WebhookHandler(config['LINE_CHANNEL_SECRET'])
+ASR_PIPE = config['ASR_PIPE']  # for Line Messaging API
+ASR_PIPE2 = config['ASR_PIPE2']  # for regular requests
+CORRECTION = config['CORRECTION']
 
 with open(config['CUSTOM_DICT'], 'rb') as f:
     custom_dict = pickle.load(f)
 
 corrector = DeepGICorrector(custom_dict, lower_case=True)
-
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
 pipe = pipeline("automatic-speech-recognition",
-                    model=asr_pipe,
+                    model=ASR_PIPE,
                     max_new_tokens=50,
                     device=device,
                     )
 
 pipe2 = pipeline("automatic-speech-recognition",
-                    model=asr_pipe2,
+                    model=ASR_PIPE2,
                     device=device,
                     )
 
@@ -59,6 +59,8 @@ async def transcribe_file(file: UploadFile = File(...)):
     file.file.seek = lambda *args: None
     audio = pydub.AudioSegment.from_file(file.file)
     text = pipe2(audio.export(format='wav').read())['text']
+    if CORRECTION:
+        text = corrector.correct(text)
     res = {
         'text': text
     }
@@ -69,17 +71,17 @@ async def transcribe_file(file: UploadFile = File(...)):
 async def callback(request: Request, x_line_signature: str = Header(None)):
     body = await request.body()
     try:
-        handler.handle(body.decode("utf-8"), x_line_signature)
+        HANDLER.handle(body.decode("utf-8"), x_line_signature)
     except InvalidSignatureError:
         raise HTTPException(status_code=400, detail="chatbot handle body error.")
     return 'OK'
 
 
-@handler.add(MessageEvent, message=AudioMessage)
+@HANDLER.add(MessageEvent, message=AudioMessage)
 def message_text(event):
-    message_content = line_bot_api.get_message_content(event.message.id)
+    message_content = LINE_BOT_API.get_message_content(event.message.id)
     text = transcribe(message_content)
-    line_bot_api.reply_message(
+    LINE_BOT_API.reply_message(
         event.reply_token,
         TextSendMessage(text=text)
     )
@@ -88,7 +90,8 @@ def message_text(event):
 def transcribe(message_content):
     audio = get_audio(message_content)
     text = pipe(audio.export(format='wav').read())['text']
-    text = corrector.correct(text)
+    if CORRECTION:
+        text = corrector.correct(text)
     return text
 
 
